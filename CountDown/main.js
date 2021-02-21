7db112d3a1c2801dc99251a22ebe2b8d207c6eee
arsh-del <60245726+arsh-del@users.noreply.github.com>
2021-02-20 21:46:54 -0800

    Add files via upload

---
+++ CountDown/main.js
@@ -0,0 +1,46 @@
+let initNum = 10, initCountdown;
+
+let colorArray = ['Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'];
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let text = document.querySelector("p");
+    let body = document.querySelector("body");
+    
+    initCountdown = setInterval(() => {
+        countdownFn(text, body)
+    }, 100)
+
+})
+
+let countdownFn = (text, body) => {
+    initNum = parseFloat(initNum - 0.1).toFixed(1)
+    if ( initNum >= 0 ) {
+
+        if ( initNum % 1 == 0 ) {
+            let randomTextColor = colorArray[Math.floor(Math.random() * colorArray.length)]
+            let randomBackgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)]
+
+            while ( randomTextColor == randomBackgroundColor ) {
+                randomBackgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)]
+            }
+
+            text.style.color = randomTextColor
+            body.style.backgroundColor = randomBackgroundColor
+        }
+
+        text.innerHTML = parseFloat(text.innerHTML - 0.1).toFixed(1)
+    } else {
+        text.innerHTML = "Take Off!"
+        clearInterval(initCountdown)
+
+        setTimeout(() => {
+            initNum = 10;
+            text.innerHTML = initNum;
+            initCountdown = setInterval(() => {
+                countdownFn(text, body)
+            }, 100)
+            console.log('reset timer')
+        }, 3000)
+    }
+}
